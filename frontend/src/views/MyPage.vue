6fd232cb5b8de91947dcb80ce424588259930da7
김준형 <licence22@naver.com>
2024-01-31 12:15:13 +0900

    fix: 마이페이지 성취율까지

--- frontend/src/views/MyPage.vue
+++ frontend/src/views/MyPage.vue
@@ -2,22 +2,26 @@
     <div class="mt-5">
       <UserInfo :userData="userData" />
       <SuccessRate :successRate="successRate" />
+      <GoalSuccessRate :goalRate="goalRate" />
     </div>
 </template>
 
 <script>
 import UserInfo from '@/components/MyPage/UserInfo.vue'
 import SuccessRate from '@/components/MyPage/SuccessRate.vue'
+import GoalSuccessRate from '@/components/Mypage/GoalSuccessRate.vue'
 
 export default {
 components: {
     UserInfo,
-    SuccessRate
+    SuccessRate,
+    GoalSuccessRate,
 },
 data() {
     return {
     userData: null,
     successRate: null,
+    goalRate: null,
     };
 },
 mounted() {
