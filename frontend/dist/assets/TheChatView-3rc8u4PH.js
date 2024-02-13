7e8e88a63acdac327933c34f1256a950e3627367
dohwi <ehgnl97722@ajou.ac.kr>
2024-02-13 14:38:39 +0900

    Chore: Chat.vue import문 삭제

--- frontend/dist/assets/TheChatView-3rc8u4PH.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,b as o,d as c,e as n,y as r}from"./index-Bw9y3RlC.js";const s={},a={class:"container text-center mt-3"};function _(i,d){const e=o("router-view");return c(),n("div",a,[r(e)])}const f=t(s,[["render",_]]);export{f as default};
