906ac8b0fd9e5490075babfd8414bdba29c4b541
김광연 <kwang9705@naver.com>
2024-02-14 11:13:40 +0900

    Fix: debug 모드 해제

--- app2.py
+++ app2.py
@@ -269,5 +269,5 @@ current_directory = os.getcwd()
 print("현재 작업 디렉토리:", current_directory)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(host='0.0.0.0', port=5050)
     
