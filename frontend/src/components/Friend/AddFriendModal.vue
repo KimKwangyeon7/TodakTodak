be7328b91bbb1372b40056c549d5120d895b875b
kennedy1234 <kennedy1234@naver.com>
2024-02-15 13:34:21 +0900

    Feat: Friend request accept

--- frontend/src/components/Friend/AddFriendModal.vue
+++ frontend/src/components/Friend/AddFriendModal.vue
@@ -1,276 +1,187 @@
 <template>
-    <div class="modal-content" style="border-radius: 10px">
-      <div class="modal-title">
-        <button type="button" class="btn-close" aria-label="Close" @click="closeModal"
-        ></button>
-      </div>
-      <form>
-        <!-- 상속 목표 -->
-        <div class="form-group">
-          <label for="selectedGoal">목표:</label>
-          <select v-model="selectedGoal" id="selectedGoal" class="form-control">
-            <option v-for="goal in goals" :key="goal.id" :value="goal">
-              {{ goal.content }}
-            </option>
-          </select>
-        </div>
-        <!-- 제목 입력 -->
-        <div class="form-group">
-          <label for="todoTitle">제목:</label>
-          <input
-            v-model="todoTitle"
-            type="text"
-            id="todoTitle"
-            class="form-control"
-            required
-          />
-        </div>
-  
-        <!-- 내용 입력 -->
-        <div class="form-group">
-          <label for="todoContent">내용:</label>
-          <textarea
-            v-model="todoContent"
-            id="todoContent"
-            class="form-control"
-            rows="3"
-          ></textarea>
-        </div>
-  
-        <!-- is_important -->
-        <div class="form-group">
-          <label>중요여부:</label>
-          <div class="form-check form-switch">
-            <input
-              v-model="important"
-              class="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="importantSwitch"
-            />
-            <label class="form-check-label" for="importantSwitch"></label>
-          </div>
-        </div>
-  
-        <!-- isOutside -->
-        <div class="form-group">
-          <label>외출 여부:</label>
-          <div class="form-check form-switch">
-            <input
-              v-model="outside"
-              class="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="outsideSwitch"
-            />
-            <label class="form-check-label" for="outsideSwitch"></label>
-          </div>
-        </div>
-  
-        <!-- 알람 여부 선택 -->
-        <!-- isAlarmed -->
-        <div class="form-group">
-          <label>알람 여부:</label>
-          <div class="custom-control custom-switch">
-            <div class="form-check form-switch">
-              <input
-                v-model="alarmed"
-                class="form-check-input"
-                type="checkbox"
-                role="switch"
-                id="flexSwitchCheckChecked"
-                checked
-              />
-              <label
-                class="form-check-label"
-                for="flexSwitchCheckChecked"
-              ></label>
-            </div>
-          </div>
-        </div>
-  
-        <!-- 알람 시간 입력 -->
-        <div class="form-group" v-if="alarmed">
-          <label for="time">알람 시간:</label>
-          <input v-model="time" type="time" id="time" class="form-control" />
-        </div>
-  
-        <button
-          type="submit"
-          class="btn todo-save"
-          @click.prevent="registerTodo()"
-        >
-          저장
-        </button>
-      </form>
+  <div class="modal-content" style="border-radius: 10px">
+    <div class="modal-title">
+      <button type="button" class="btn-close" aria-label="Close" 
+              @click="closeModal">
+      </button>
     </div>
-  </template>
-  
-  <script>
-  import { addTodo } from "@/api/todos";
-  import { getGoalList } from "@/api/goals";
-  
-  export default {
-    data() {
-      return {
-        // todo-list
-        selectedGoal: null,
-        todoId: "",
-        todoTitle: "",
-        todoContent: "",
-        todoDate: "",
-        // alarm(알람 설정할 때만 필요한 영역)
-        day: "",
-        time: "",
-        important: false,
-        outside: false,
-        alarmed: false,
-        checked: false,
-        completed: false,
-        goals: [],
-      };
-    },
-  
-    methods: {
-      closeModal() {
-        this.$emit("close-modal");
-      },
-      registerTodo() {
-        try {
-          const now = new Date();
-          var year = now.getFullYear().toString();
-          var month = (now.getMonth() + 1).toString();
-          var day = now.getDate().toString();
-          if (month < 10) {
-            month = "0" + month;
-          }
-  
-          if (day < 10) {
-            day = "0" + day;
-          }
-          const todoDate = year + "" + month + "" + day;
-          const alarmTime = this.fourDigitTime(this.time);
-  
-          console.log("todoDate:", todoDate);
-  
-          const goalColor = this.selectedGoal.color;
-          console.log("goalColor:", goalColor);
-          const goalId = this.selectedGoal.id;
-          console.log("goalId:", goalId);
-          // selectedGoal 객체에서 goalId를 가져옵니다.
-          const todo = {
-            title: this.todoTitle, // 제목
-            content: this.todoContent, // 내용
-            color: goalColor, // 색상
-            important: this.important, // 중요여부
-            outside: this.outside, // 외출여부
-            alarmed: this.alarmed, // 알람여부
-            time: alarmTime, // 알람시간
-          };
-  
-          try {
-            addTodo(goalId, todo, todoDate,
-              ({ data }) => {
-                console.log("Todo added:", data);
-                console.log(data);
-              },
-              (error) => {
-                console.log(error);
-              }
-            );
-            console.log("goals", this.goals);
-            // this.goals = await getGoalList();
-          } catch (error) {
-            console.error("Error fetching goals:", error);
-          }
-        } catch (error) {
-          console.error("Error creating todo:", error);
-        }
-      },
-      async fetchGoals() {
-        console.log("fetchGoals 실행");
-        try {
-          getGoalList(
-            ({ data }) => {
-              console.log("목표리스트");
-              console.log(data);
-              this.goals = data;
-            },
-            (error) => {
-              console.log(error);
-            }
-          );
-          console.log("goals", this.goals);
-          // this.goals = await getGoalList();
-        } catch (error) {
-          console.error("Error fetching goals:", error);
-        }
-      },
-  
-      fourDigitTime(t) {
-        const [hours, minutes] = t.split(":");
-        return hours + minutes;
-      },
-  
-      eightDigitDate(d) {
-        const currentDate = new Date();
-        const yyyy = currentDate.getFullYear();
-        const mm = String(currentDate.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 1을 더하고 2자리로 만듭니다.
-        const dd = String(currentDate.getDate()).padStart(2, "0"); // 날짜를 2자리로 만듭니다.
-        const curDate = `${yyyy}${mm}${dd}`;
-        return curDate;
-      },
-  
-      fourDigitTime(t) {
-        const [hours, minutes] = t.split(":");
-        return hours + minutes;
-      },
-    },
-  
-    mounted() {
-      this.fetchGoals();
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .modal-content {
-    background: #fff; /* 나머지 부분은 하얀색 배경 */
-  }
-  
-  .modal-title {
-    background: #eaf3f9; /* 제목 부분에만 #EAF3F9 배경 색상 적용 */
-    padding: 10px; /* 여백 추가 */
-    border-radius: 8px;
-    margin-bottom: 20px; /* 여백 추가 */
-    display: flex; /* Flexbox 사용 */
-    align-items: center; /* 수직 가운데 정렬 */
-  }
-  
-  .btn-close {
-    width: 20px;
-    height: 20px;
-    background-color: #eaf3f9; /* 배경 색상 수정 */
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    font-size: 12px;
-    margin-right: auto; /* 나머지 공간을 최대한 차지하여 왼쪽으로 이동 */
-  }
-  .form-group {
-    text-align: left;
-    margin-left: 10px;
-    margin-right: 10px;
-  }
-  
-  .custom-control-label {
-    padding-left: 10px;
-  }
-  
-  .todo-save {
-      display: flex;
-      margin-left: auto;
-    }
-  
-  </style>
-  
+    <div
+        v-for="request in friendRequests"
+        :key="request.id"
+        class="friend-request"
+      >
+        <div class="request-info">
+          <img
+            :src="request.profilePicture"
+            alt="프로필 사진"
+            class="profile-picture"
+          />
+          <div class="request-details">
+            <div class="request-memo">{{ request.nickname }}</div>
+            <div class="request-memo">{{ request.memo }}</div>
+          </div>
+        </div>
+        <div class="request-buttons">     
+          <button class="accept-button" @click="acceptFriend(request.nickname)">확인</button>
+          <button class="reject-button" @click="rejectFriend(request.nickname)">삭제</button>
+        </div> 
+      </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineEmits, onMounted } from 'vue';
+import { acceptFriends } from '@/api/friend'
+import { acceptFriendRequest } from "@/api/friend";
+import { rejectFriendRequest } from "@/api/friend";
+
+onMounted(() => {
+  sendToMeAcceptFriends()
+  // 컴포넌트가 마운트된 후의 추가 로직을 여기에 작성
+});
+
+// 'close-modal' 이벤트를 정의합니다.
+const emit = defineEmits(['close-modal']);
+
+// 모달을 닫는 함수
+function closeModal() {
+  emit('close-modal'); // 'close-modal' 이벤트를 발생시킵니다.
+}
+
+const friendRequests = ref(null);
+
+// onMounted(async () => {
+//   try {
+//     const response = await acceptFriends(); // friendRequests API 호출
+//     console.log('friendData:', response.data);
+//     friendRequests.value = response.data; // friendRequests는 반응형 참조일 것으로 가정
+//     // 이 경우 friendRequests.value에 API 응답의 데이터를 할당합니다.
+//   } catch (error) {
+//     console.error("친구 요청 데이터 로딩 중 오류 발생:", error);
+//   }
+// });
+
+const sendToMeAcceptFriends = async () => {
+  acceptFriends(({ data }) => {
+    console.log("data: ", data);
+    friendRequests.value = data;
+    friendRequests.value.push(data);
+    console.log("friendRequests: ", friendRequests.value);
+  });
+};
+
+const acceptfriendRequests = ref(false)
+
+const acceptFriend = async (nickname) => {
+  acceptFriendRequest(nickname, ({ data }) => {
+    acceptfriendRequests.value = true;
+    console.log("data: ", data);
+  });
+};
+
+const rejectFriend = async (friendNickname) => {
+  rejectFriendRequest(friendNickname, ({ data }) => {
+    acceptfriendRequests.value = false;
+    console.log("data: ", data);
+  });
+};
+
+</script>
+
+<style scoped>
+.modal-content {
+  background: #fff; /* 나머지 부분은 하얀색 배경 */
+}
+
+.modal-title {
+  background: #eaf3f9; /* 제목 부분에만 #EAF3F9 배경 색상 적용 */
+  padding: 10px; /* 여백 추가 */
+  border-radius: 8px;
+  margin-bottom: 20px; /* 여백 추가 */
+  display: flex; /* Flexbox 사용 */
+  align-items: center; /* 수직 가운데 정렬 */
+}
+
+.btn-close {
+  width: 20px;
+  height: 20px;
+  background-color: #eaf3f9; /* 배경 색상 수정 */
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 12px;
+  margin-left: auto; /* 나머지 공간을 최대한 차지하여 왼쪽으로 이동 */
+}
+.form-group {
+  text-align: left;
+  margin-left: 10px;
+  margin-right: 10px;
+}
+
+.custom-control-label {
+  padding-left: 10px;
+}
+
+.todo-save {
+    display: flex;
+    margin-left: auto;
+}
+
+.friend-request {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: #f9f9f9;
+  border-radius: 8px;
+}
+
+.request-info {
+  display: flex;
+  align-items: center;
+}
+
+.profile-picture {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+
+
+.request-details {
+  display: flex;
+  flex-direction: column;
+}
+
+.request-name {
+  font-weight: bold;
+}
+
+.request-memo {
+  color: gray;
+}
+
+.request-buttons {
+  display: flex; /* 추가 */
+}
+
+.accept-button,
+.reject-button {
+  padding: 5px 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.accept-button {
+  background-color: #28a745;
+  color: white;
+}
+
+.reject-button {
+  background-color: #dc3545;
+  color: white;
+}
+</style>
--- frontend/src/components/Todo/AddTodo.vue
+++ frontend/src/components/Todo/AddTodo.vue
@@ -132,6 +132,7 @@ export default {
   },
 
   methods: {
+    
     closeModal() {
       this.$emit("close-modal");
     },
